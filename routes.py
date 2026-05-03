ae4306c3a21271a18554813e2b35a7674f338f3a
Matěj Klíma <klima.matej@student.gymzr.cz>
2026-05-03 15:03:37 +0200

    úprava formuláře

--- routes.py
+++ routes.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def kalkulacka_trojuhelniku():
-    return render_template("page.html", polozky=zobrazeni.zbr.polozky)
+    return render_template("page.html", zobrazeni=zobrazeni.zbr)
 
 if __name__ == "__main__":
     app.run(debug=True)
