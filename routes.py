0599595b8e9122fe1dec0f0ed733c4f1d706fb39
Matěj Klíma <klima.matej@student.gymzr.cz>
2026-05-03 14:44:12 +0200

    přidány cesty, html template a zobrazení

--- routes.py
+++ routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
+import zobrazeni
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def kalkulacka_trojuhelniku():
-    return render_template("page.html")
+    return render_template("page.html", polozky=zobrazeni.zbr.polozky)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ zobrazeni.py
@@ -0,0 +1,37 @@
+from collections import namedtuple
+
+Pole = namedtuple("Pole", ["promena", "pozice", "nazev", "name"])
+
+class Zobrazeni:
+    def __init__(self, polozky):
+        self.polozky = []
+        self.zarad(polozky)
+        print(self.polozky)
+
+    def zarad(self, polozky : dict):
+        for promena, nazvy in polozky.items():
+            if isinstance(nazvy, tuple):
+                assert(len(nazvy) == 3)
+                for i, nazev in enumerate(nazvy):
+                    self.vytvor_polozku(promena, i, nazev)
+            else:
+                self.vytvor_polozku(promena, 0, nazvy)
+
+    def vytvor_polozku(self, promena, pozice, nazev):
+        assert(isinstance(promena, str))
+        assert(isinstance(nazev, str))
+        
+        self.polozky.append(Pole(
+            promena,
+            pozice,
+            nazev,
+            promena + str(pozice)
+        ))
+
+zbr = Zobrazeni(
+    {
+        "strana" : ("a", "b", "c"),
+        "uhel" : ("α", "β", "γ"),
+        "r" : "r",
+    }
+)
