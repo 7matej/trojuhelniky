74df993025998c7e727f2e114cdff7746eaaf693
Matěj Klíma <klima.matej@student.gymzr.cz>
2026-05-05 22:08:22 +0200

    vylepšený test

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from formular import Formular
 
 
 #test konzistence pravidel
-resic = TrojuhelnikovyResic(rs)
+resic = TrojuhelnikovyResic(rs, print_error=False)
 resic.set("strana", 0, 8)
 resic.set("strana", 1, 9)
 resic.set("strana", 2, 12)
@@ -30,29 +30,40 @@ formular = Formular(
     "strana" : ("a", "b", "c"),
     "uhel" : ("α", "β", "γ"),
     "r" : "r",
+    "S" : "S"
 }
 )
 statistiky = Counter()
 
 for trojice in combinations(formular.polozky, 3):
-    
-    resic = TrojuhelnikovyResic(rs)
-    runner = Runner(resic)
-    
-    for pole in trojice:
-        resic.set(pole["promena"], pole["pozice"], uniform(20, 70))
-    
-    runner.run(ban)
 
-    statistiky[runner.stav] += 1
-    if runner.stav == State.Unsolved:
-        print("Chyba:")
+    for i in range(10):                 #počet pokusů
+        resic = TrojuhelnikovyResic(rs, print_error=False)
+        runner = Runner(resic)
+        
         for pole in trojice:
-            print(pole["nazev"])
-        raise AssertionError
+            resic.set(pole["promena"], pole["pozice"], uniform(20, 70))
+        
+        #spuštění výpočtu
+        runner.run(ban)
 
-    for res in runner.vysledky:
-        res.test()
+        statistiky[runner.stav] += 1
+        
+        if runner.stav == State.Unsolved:
+            print("Chyba:")
+            print(resic.promene)
+            print(*(pole["nazev"] for pole in trojice), sep=", ")
+            raise AssertionError("Trojúhelník se nepodařilo vyřešit.")
+
+        for res in runner.vysledky:
+            res.test()
+
+        if runner.stav != State.Failure:
+            break
+    else:
+        print("Varování:")
+        print(*(pole["nazev"] for pole in trojice), sep=", ")
+        print("Nepodařilo se najít existující trojúhelník.")
 
 print(statistiky)
 print("✅")
